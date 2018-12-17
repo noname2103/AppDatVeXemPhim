19f8ef084aa57a58fc40312ad70e66f3a506bace
trungminhminh <38715621+trungminhminh@users.noreply.github.com>
2018-12-17 13:14:02 +0700

    Database Khachhang

---
+++ app/src/main/java/com/dxt/xuantruong/appdatvexemphim/KhachangAdapter.java
@@ -0,0 +1,41 @@
+package com.dxt.xuantruong.appdatvexemphim;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.List;
+
+public class KhachangAdapter extends BaseAdapter {
+    private Context context;
+    private int layout;
+    private List<Khachhang> khachhangList;
+
+    public KhachangAdapter(Context context, int layout, List<Khachhang> khachhangList) {
+        this.context = context;
+        this.layout = layout;
+        this.khachhangList = khachhangList;
+    }
+
+    @Override
+    public int getCount() {
+        return khachhangList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
