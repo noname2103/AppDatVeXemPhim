14e3affe26c6c652b04a744dde14de8bd9a22fa3
noname2103 <35282818+noname2103@users.noreply.github.com>
2018-12-17 12:36:22 +0700

    fix bug made by all
    fix bug

--- app/src/main/java/com/dxt/xuantruong/appdatvexemphim/DangKyActivity.java
+++ app/src/main/java/com/dxt/xuantruong/appdatvexemphim/DangKyActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class DangKyActivity extends AppCompatActivity {
 
@@ -12,12 +13,16 @@ public class DangKyActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dang_ky);
 
-        //Button LoginBtn = (Button)findViewById(R.id.signUpBtn);
-        //LoginBtn.setOnClickListener(DangKyActivity.this);
+        Button LoginBtn = (Button)findViewById(R.id.signUpBtn);
+        LoginBtn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                //String tc = "Thanh cong";
+                //Toast.makeText(this, tc, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
-    public void onClick(View v)
-    {
 
-    }
 }
