407fa7bb6af398ba716242e1c4bdd37892a3152c
noname2103 <35282818+noname2103@users.noreply.github.com>
2018-12-17 15:49:30 +0700

    dangky & dangnhap
    dangky & dangnhap

--- app/src/main/java/com/dxt/xuantruong/appdatvexemphim/LoginActivity.java
+++ app/src/main/java/com/dxt/xuantruong/appdatvexemphim/LoginActivity.java
@@ -14,9 +14,7 @@ public class LoginActivity extends AppCompatActivity {
 
     Database database;
 
-    EditText edituser,editpass;
-    Button btnlogin;
-    TextView tvcreate,tvforgot;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +29,11 @@ public class LoginActivity extends AppCompatActivity {
         Cursor dataKH = database.GetData("SELECT * FROM KhachHang");
         while (dataKH.moveToNext())
         {
-            String ten = dataKH.getString(1);
-            Toast.makeText(this, ten, Toast.LENGTH_SHORT).show();
+            String sdt = dataKH.getString(3);
+            String pass = dataKH.getString(5);
+            Toast.makeText(this, "sdt: "+sdt +"/pass: "+pass, Toast.LENGTH_SHORT).show();
         }
-        anhxa();
-        ControllButton();
+
         TextView register = (TextView)findViewById(R.id.createAccount);
         register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,18 +42,47 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        Button loginbtn = (Button)findViewById(R.id.loginBtn);
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText sdt2 = (EditText)findViewById(R.id.login_phone);
+                String sdt = sdt2.getText().toString();
+                EditText matkhau1 = (EditText)findViewById(R.id.login_password);
+                String matkhau = matkhau1.getText().toString();
+                Button loginbtn = (Button)findViewById(R.id.loginBtn);
+
+                int flat = 0;
+                Cursor dataKH1 = database.GetData("SELECT * FROM KhachHang WHERE Sdt LIKE '" + sdt + "' AND MatKhau LIKE '"+ matkhau +"'");
+//                while (dataKH1.moveToNext())
+//                {
+//                    String ten = dataKH1.getString(3);
+//                    Toast.makeText(LoginActivity.this, ten, Toast.LENGTH_SHORT).show();
+//                }
+                while (dataKH1.moveToNext())
+                {
+                    String ten = dataKH1.getString(3);
+                    if(ten != null)
+                    {
+                        flat = 1;
+                        break;
+                    }
+                    //Toast.makeText(LoginActivity.this, ten, Toast.LENGTH_SHORT).show();
+                }
+                //Toast.makeText(LoginActivity.this, sdt+ ":" + matkhau, Toast.LENGTH_SHORT).show();
+                if(flat == 1)
+                {
+                    Intent intent = new Intent(LoginActivity.this, IndexActivity.class);
+                    startActivity(intent);
+                }
+                else
+                {
+                    Toast.makeText(LoginActivity.this, "Đăng nhập thất bại", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
-    private void ControllButton() {
 
-    }
-
-    private void anhxa() {
-        edituser = (EditText)findViewById(R.id.login_user);
-        editpass = (EditText)findViewById(R.id.login_password);
-        btnlogin = (Button)findViewById(R.id.loginBtn);
-        tvcreate = (TextView)findViewById(R.id.createAccount);
-        tvforgot = (TextView)findViewById(R.id.forgot_password);
-
-    }
 }
